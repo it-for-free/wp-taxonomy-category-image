8f669c17bc8749d15222e8fbc0d410d82848c705
vedro-compota <ru-ibn-hottab@yandex.ru>
2019-03-28 16:19:14 +0300

    init

---
+++ wp-taxonomy-category-image.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+  Plugin Name: Изображение для категории таксономии
+  Plugin URI: https://github.com/it-for-free/wp-taxonomy-category-image
+  Description: Добавит возможность добавлять изображение к элементам таксономии // Taxonomy Category Image Field
+  Version: 0.0.1
+  Author: vedro-compota
+  Author URI: https://github.com/it-for-free/wp-taxonomy-category-image
+ */
+
